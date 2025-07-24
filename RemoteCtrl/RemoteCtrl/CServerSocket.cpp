586dd093064e08b534ccc2bee9fbaf6627b430c9
ChaofanChen-fanren <2536831142@qq.com>
2025-07-24 20:24:54 +0800

    finish CPacket class

--- RemoteCtrl/RemoteCtrl/CServerSocket.cpp
+++ RemoteCtrl/RemoteCtrl/CServerSocket.cpp
@@ -63,7 +63,7 @@ bool CServerSocket::AcceptClient()
 
 int CServerSocket::DealCommand()
 {
-	if (m_client == NULL) return -1;
+	if (m_client == INVALID_SOCKET) return -1;
 	char* buffer = new char[BUFFER_SIZE];
 	memset(buffer, 0, BUFFER_SIZE);
 	size_t index = 0;
@@ -74,10 +74,16 @@ int CServerSocket::DealCommand()
 		}
 		index += len;
 		len = index;
-		// 解析命名
+
+		//TODO: 解析命令
+		m_packet = CPacket((BYTE*)buffer, len);
+		if (len > 0) {
+			memmove(buffer, buffer + len, BUFFER_SIZE - len);
+			index -= len;
+			return m_packet.sCmd; // 接受成功, 返回命令
+		}
 	}
-	
-	return 0;
+	return -1;
 }
 
 bool CServerSocket::Send(const char* pData, int nSize)
@@ -93,4 +99,103 @@ CServerSocket* CServerSocket::getInstance()
 		m_instance = new CServerSocket();
 	}
 	return m_instance;
+}
+
+CPacket::CPacket(WORD nCmd, const BYTE* pData, size_t nSize)
+{
+	sHead = 0xFEFF;
+	nLength = nSize + 4;
+	sCmd = nCmd;
+	if (nSize > 0) {
+		strData.resize(nSize);
+		memcpy((void*)strData.c_str(), pData, nSize);
+	}
+	else {
+		strData.clear(); // 无数据清空
+	}
+	sSum = 0;
+	for (size_t i = 0; i < strData.size(); i++) {
+		sSum += BYTE(strData[i]) & 0xFF;
+	}
+}
+
+CPacket::CPacket(const CPacket& pack)
+{
+	sHead = pack.sHead;
+	nLength = pack.nLength;
+	sCmd = pack.sCmd;
+	strData = pack.strData;
+	sSum = pack.sCmd;
+}
+
+CPacket& CPacket::operator=(const CPacket& pack)
+{
+	if (this != &pack) {
+		sHead = pack.sHead;
+		nLength = pack.nLength;
+		sCmd = pack.sCmd;
+		strData = pack.strData;
+		sSum = pack.sCmd;
+	}
+
+	return *this;
+}
+
+CPacket::CPacket(const BYTE* pData, size_t& nSize) // 用于包数据解析
+{
+	size_t i = 0;
+	for (; i < nSize; i++) {
+		if (*(WORD*)(pData + i) == 0xFEFE) {  // 识别包头
+			sHead = *(WORD*)(pData + i); // 取包头
+			i += 2;
+			break;
+		}
+	}
+	if (i + 4 + 2 + 2 > nSize) // 包数据可能不全
+	{
+		nSize = 0;
+		return; // 解析失败
+	}
+	nLength = *(DWORD*)(pData + i); i += 4; // 读长度
+	if (nLength + i > nSize) 
+	{
+		nSize = 0;
+		return; // 解析失败
+	}
+
+	sCmd = *(WORD*)(pData + i); i += 2; // 读命令类型
+
+	if (nLength > 4) { // 数据长度大于4
+		strData.resize(nLength - 2 - 2);
+		memcpy((void *)strData.c_str(), pData + i, nLength - 2 - 2);
+		i = nLength - 4;
+	}
+
+	sSum = *(WORD*)(pData + i); i += 2;// 接受校验码
+	WORD sum = 0;
+	for (size_t j = 0; j < strData.size(); j++) {
+		sum += BYTE(strData[j]) & 0xFF;
+	}
+	if (sum == sSum) {     // 校验
+		nSize = i;         // 头 + 长度+ 数据
+		return;
+	}
+	nSize = 0;
+}
+
+int CPacket::Size() // 获取包数据长度
+{
+	return nLength + 6;
+}
+
+const char* CPacket::Data() // 用于解析包，将包送入缓冲区
+{
+	strOut.resize(Size());
+	BYTE* pData = (BYTE*)strOut.c_str();
+	*(WORD*)pData = sHead; pData += 2;
+	*(DWORD*)pData = nLength; pData += 4;
+	*(WORD*)pData = sCmd; pData += 2;
+	memcpy(pData, strData.c_str(), strData.size()); pData += strData.size();
+	*(WORD*)pData = sSum;
+	return strOut.c_str(); 
 }
--- RemoteCtrl/RemoteCtrl/CServerSocket.h
+++ RemoteCtrl/RemoteCtrl/CServerSocket.h
@@ -2,6 +2,28 @@
 #include "pch.h"
 #include "framework.h"
 
+class CPacket
+{
+public:
+	CPacket() : sHead(0), nLength(0), sCmd(0), sSum(0) {};
+	CPacket(WORD nCmd, const BYTE* pData, size_t nSize);
+	CPacket(const CPacket& pack);
+	CPacket& operator= (const CPacket& pack);
+	CPacket(const BYTE* pData, size_t& nSize);
+	~CPacket() {};
+
+	int Size();
+	const char* Data();
+
+public:
+	WORD sHead;              // 包头, 固定为FE FF
+	DWORD nLength;           // 包长
+	WORD sCmd;               // 命令
+	std::string strData;     // 包数据
+	WORD sSum;               // 和校验
+
+	std::string strOut;      // 整个包的数据
+};
 
 
 class CServerSocket
@@ -25,6 +47,7 @@ private:
 	SOCKET m_client;
 	static CServerSocket *m_instance;
 	static CHelper m_helper; // 帮助释放m_instance的对象
+	CPacket m_packet;
 
 	class CHelper
 	{
